24cc3b82e8c18bf8b82a518eef277e928da5abbd
olefire <109245281+olefire@users.noreply.github.com>
2022-11-19 00:13:51 +0300

    v1

--- main.js
+++ main.js
@@ -1,66 +1,68 @@
-window.addEventListener("DOMContentLoaded", function(){
+window.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("popstate", formShow);
-    let buttonFormShow=document.getElementById("buttonFormShow");
-    buttonFormShow.addEventListener("click",function (){
-        history.pushState({"form": true},"","?form=true");
+    let buttonFormShow = document.getElementById("buttonFormShow");
+    buttonFormShow.addEventListener("click", function () {
+        history.pushState({"form": true}, "", "?form=true");
         formShow();
     });
     formShow();
-    let inputs=document.querySelectorAll(".save-to-storage");
-    inputs.forEach( function(input) {input.addEventListener("input", saveData);} );
-    let formButton=document.getElementById("formSubmit");
-    formButton.addEventListener("click",sendForm);
+    let inputs = document.querySelectorAll(".save-to-storage");
+    inputs.forEach(function (input) {
+        input.addEventListener("input", saveData);
+    });
+    let formButton = document.getElementById("formSubmit");
+    formButton.addEventListener("click", sendForm);
 });
 
-function formShow(){
-    let popup=document.getElementById("popup");
-    if(history.state!=null && history.state.form===true) {
-        popup.style.display="block";
-        popup.style.position="absolute";
-        let inputs=document.querySelectorAll(".save-to-storage");
-        inputs.forEach( function(input){ input.value=localStorage.getItem(input.id); } );
-    }
-    else {
-        popup.style.display="none";
+function formShow() {
+    let popup = document.getElementById("popup");
+    if (history.state !== null && history.state.form === true) {
+        popup.style.display = "block";
+        popup.style.position = "absolute";
+        let inputs = document.querySelectorAll(".save-to-storage");
+        inputs.forEach(function (input) {
+            input.value = localStorage.getItem(input.id);
+        });
+    } else {
+        popup.style.display = "none";
     }
 }
 
-function saveData(){
-    localStorage.setItem(this.id,this.value);
+function saveData() {
+    localStorage.setItem(this.id, this.value);
 }
 
-function sendForm(){
-    let name=document.getElementById("name").value;
-    let email=document.getElementById("email").value;
-    let message=document.getElementById("message").value;
-    let checkbox=document.getElementById("checkbox");
-    let result=document.getElementById("result");
-    if (name!="" && message!="" && checkbox.checked && validateEmail(email)!=null) {
+function sendForm() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let checkbox = document.getElementById("checkbox");
+    let result = document.getElementById("result");
+    if (name != "" && message != "" && checkbox.checked && validateEmail(email) != null) {
         let sendRequest = new XMLHttpRequest();
 
-        sendRequest.open('POST', 'https://formcarry.com/s/cB6SsjsbV');
+        sendRequest.open('POST', 'https://formcarry.com/s/jBM7nj5uO');
         sendRequest.setRequestHeader('Content-Type', 'application/json');
         sendRequest.setRequestHeader('Accept', 'application/json');
-        let popupForm={"name": name,"email": email,"message": message};
+        let popupForm = {"name": name, "email": email, "message": message};
         sendRequest.send(JSON.stringify(popupForm));
-        let nameObj=document.getElementById("name");
-        nameObj.value="";
-        let emailObj=document.getElementById("email");
-        emailObj.value="";
-        let messageObj=document.getElementById("message");
-        messageObj.value="";
-        checkbox.checked=false;
-        sendRequest.onreadystatechange = function(){
-            if(this.readyState == 4){
+        let nameObj = document.getElementById("name");
+        nameObj.value = "";
+        let emailObj = document.getElementById("email");
+        emailObj.value = "";
+        let messageObj = document.getElementById("message");
+        messageObj.value = "";
+        checkbox.checked = false;
+        sendRequest.onreadystatechange = function () {
+            if (this.readyState === 4) {
                 localStorage.clear();
-                result.style.color="#2718dc";
-                result.innerHTML="Успешно отправлено";
+                result.style.color = "blue";
+                result.innerHTML = "Success";
             }
         }
-    }
-    else {
-        result.style.color="red";
-        result.innerHTML="Заполните форму полностью и отметьте чекбокс";
+    } else {
+        result.style.color = "red";
+        result.innerHTML = "Заполните форму полностью и отметьте чекбокс";
     }
 }
 
